b038ae097a4ce4ea42a3b0a1f00374f13876b460
fahadshuvo16 <fahadshuvo.fs@gmail.com>
2022-08-26 18:21:14 +0600

    Add project files.

---
+++ MovieIndexWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieIndexWeb.Models;
+
+namespace MovieIndexWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories{ get; set; }
+    }
+}
---
+++ MovieIndexWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieIndexWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
